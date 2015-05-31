6f629830457321ad18696ac012d0b3ce46ac666f
meads58 <meads109@gmail.com>
2015-05-31 21:47:39 +0100

    splitting up the selector message into tag, id and class components. Functions to find elements by id and class are working. Need to get the tag from the selector message and then create an order of precedence on these functions.

--- Answer.js
+++ Answer.js
@@ -1,13 +1,55 @@
 var $ = function (selector) {
+  // selector = "div#some_id.some_class"
   var elements = [];
-  var id_class = [];
+  var tag = '';
+  var idName = '';
+  var className = '';
+  var selectorLength = selector.length
 
-  id_class.push(selector.search(/\./));
-  id_class.push(selector.search('#'));
+  var idIndex = selector.search('#');
+  var classIndex = selector.search(/\./);
+
+  var findTag = function(){
+
+  }
+
+  var findId = function(){
+    if (idIndex > -1){
+      if (classIndex > idIndex){
+        selectorLength = classIndex
+      }
+
+      var id_val = selector.slice(idIndex + 1, selectorLength )
+      elements.push(document.getElementById(id_val));
+    }else{
+      return ''
+    }
+  };
+
+  var findClass = function(){
+    if (classIndex > -1){
+      if (idIndex > classIndex){
+        selectorLength = idIndex
+      }
+      return selector.slice(classIndex + 1, selectorLength )
+    }else{
+      return ''
+    }
+  }
+
+  idName = findId()
+  className = findClass()
+
+//elements = (document.getElementsByClassName(className));
+
+//elements = (document.getElementById('some_id'));
+console.log(document.getElementsByClassName(className))
+
+
+
+
   ///////////////////
   // Your code here //
   ////////////////////
-  console.log(id_class.sort())
-  console.log(selector.split('#'))
   return elements;
 }
