f220cff2135a76caf6843a5e97ea829f7957bc2f
meads58 <meads109@gmail.com>
2015-06-03 09:40:22 +0100

    successfully splitting the selector up into tag, Id and class elements and putting these into an array

--- Answer.js
+++ Answer.js
@@ -1,55 +1,153 @@
+"use strict";
 var $ = function (selector) {
-  // selector = "div#some_id.some_class"
+  var selector = "div.me.to#cool"
   var elements = [];
   var tag = '';
   var idName = '';
-  var className = '';
-  var selectorLength = selector.length
+  var className = [];
+  var selectorArray = []
+  var selectorLength = 0;
+  var classIdIndexPos = [];
 
-  var idIndex = selector.search('#');
-  var classIndex = selector.search(/\./);
+  var position = {
+    idIndex: selector.search('#'),
+    classIndex: selector.search(/\./)
+   };
 
-  var findTag = function(){
-
+  var compareNumbers = function(a, b) {
+    return a - b;
   }
 
-  var findId = function(){
-    if (idIndex > -1){
-      if (classIndex > idIndex){
-        selectorLength = classIndex
-      }
+  var findIdIndex = function() {
+    var idIndexPos = selector.search('#')
+    if (idIndexPos > -1 ){
+      classIdIndexPos.push(idIndexPos)
+    }
+  }
 
-      var id_val = selector.slice(idIndex + 1, selectorLength )
-      elements.push(document.getElementById(id_val));
-    }else{
-      return ''
+  var findClassIndexes = function (){
+    for(var i=0; i<selector.length;i++) {
+      if (selector[i] === "."){
+        classIdIndexPos.push(i);
+      }
     }
   };
 
-  var findClass = function(){
-    if (classIndex > -1){
-      if (idIndex > classIndex){
-        selectorLength = idIndex
-      }
-      return selector.slice(classIndex + 1, selectorLength )
+  var sliceUpSelector = function() {
+    findIdIndex();
+    findClassIndexes();
+    if (classIdIndexPos.length > 0){
+      sliceUp()
     }else{
-      return ''
-    }
-  }
+      tag = selector;
+    };
+  };
 
-  idName = findId()
-  className = findClass()
+  var sliceUp = function() {
+    classIdIndexPos = classIdIndexPos.sort(compareNumbers);
+      if(classIdIndexPos[0] === 0){
+        for (var i=0; i<classIdIndexPos.length;i++){
+            if(classIdIndexPos[i + 1] < selector.length){
+              selectorArray.push(selector.slice(classIdIndexPos[i], classIdIndexPos[i+1]))
+            }else{
+              selectorArray.push(selector.slice(classIdIndexPos[i], selector.length))
+            }
+          }
+      }else{
+        selectorArray.push(selector.slice(0, classIdIndexPos[0]))
+          for (var i=0; i<classIdIndexPos.length;i++){
+            if(classIdIndexPos[i + 1] < selector.length){
+              selectorArray.push(selector.slice(classIdIndexPos[i], classIdIndexPos[i+1]))
+            }else{
+              selectorArray.push(selector.slice(classIdIndexPos[i], selector.length))
+            }
+          }
+      }
 
-//elements = (document.getElementsByClassName(className));
+  };
 
-//elements = (document.getElementById('some_id'));
-console.log(document.getElementsByClassName(className))
+  // var findTag = function(){
+  //  }
+
+
+  // var findId = function(){
+  //   if (position.classIndex > position.idIndex){
+  //     selectorLength = position.classIndex
+  //   }else{
+  //     selectorLength = selector.length
+  //   }
+  //   idName = selector.slice(position.idIndex + 1, selectorLength )
+  // };
+
+  // var findClass = function(){
+  //   if (position.classIndex > -1){
+
+  //     if (position.idIndex > position.classIndex){
+  //       selectorLength = position.idIndex
+  //     }else{
+  //       selectorLength = selector.length
+  //     }
+
+  //     className = selector.slice(position.classIndex + 1, selectorLength )
+  //   }else{
+  //     return ''
+  //   }
+  // };
+
+  // var findById = function() {
+  //   findId();
+  //   var idElement = document.getElementById(idName);
+
+  //   checkTagName(idElement);
+  //   if (checkTagName(idElement) == true ){
+  //     elements.push(idElement)
+  //   }
+  // }
+
+  // var checkTagName = function (element) {
+  //   findTag();
+
+  //   // if (element.tagName.toLowerCase() === tag){
+  //   //   return true;
+  //   // }else{
+  //   //   return false;
+  //   // };
+  // };
+
+  // var checkClassName = function (element){
+  //   findClass();
+  // };
+
+//console.log(document.getElementById(idName))
+// findById();
+//  findId();
+//  findClass();
+//  findTag();
 
 
 
+// var elements2 = (document.getElementsByClassName(className));
+
+// elements.push(document.getElementById('some_id'));
+// var checkClassList = function () {
+
+// }
+// findIdIndex()
+// findClassIndexes()
+sliceUpSelector()
+console.log(selectorArray)
+
+
+//console.log(elements[0].classList)
+//console.log(elements[0].classList)
+//console.log(elements[0].tagName)
+console.log('id name: ' + idName)
+console.log('class name: ' + className)
+console.log('tag name: ' + tag)
 
   ///////////////////
   // Your code here //
   ////////////////////
   return elements;
 }
+
---
+++ README.md
@@ -0,0 +1,4 @@
+###Approach
+split the selector into it's 'type', 'id' and 'class'
+created an order of precendence to search from id, class, type
+for each selection compare the returned type, id and class with the selector to confirm a match
