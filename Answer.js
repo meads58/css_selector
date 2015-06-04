80e794ad7f07311898d34537a4c87bf1d76f9412
meads58 <meads109@gmail.com>
2015-06-04 17:13:11 +0100

    refactoring and updating the readme file.

--- Answer.js
+++ Answer.js
@@ -1,31 +1,20 @@
 "use strict";
 var $ = function (selector) {
-  //var selector = "div.me.to#cool"
   var elements = [];
-  var tagName = '';
-  var idName = '';
-  var classNames = [];
-  var foundById = false;
-  var noClassMatch = false;
-  var selectorArray = []
-
   var classIdIndexPos = [];
   var splitUp = selector.split(/(?=[#\.])/g);
   var arrOfClasses = [];
-  var tagDiv = '';
+  var tagName = '';
   var idValue = '';
-  var idElement = ''
-  var match = false
 
-
-
-  var compareNumbers = function(a, b) {
-    return a - b;
-  }
+  var init = function() {
+    getTag();
+    getAllClasses();
+  };
 
   var getTag = function() {
     if(['#', '.'].indexOf(splitUp[0].charAt(0)) === -1){
-      tagName = splitUp[0]
+      tagName = splitUp[0];
       return true;
     }
   };
@@ -34,7 +23,7 @@ var $ = function (selector) {
     for(var i=0; i < splitUp.length; i++){
       if(['#'].indexOf(splitUp[i].charAt(0)) === 0){
         idValue = splitUp[i]
-        return true
+        return true;
       }
     }
   };
@@ -72,262 +61,96 @@ var $ = function (selector) {
   }
 
   var matchingRule = function() {
+    init()
     if (splitUp.length === 1){
-      matchOneTag()
+      oneElement()
     }else{
-      multipleTags()
-      console.log('err1')
+      multiElements()
     }
   }
 
-  var matchOneTag= function() {
-    if(getTag() === true){
+  var oneElement= function() {
+    if (getTag()) {
       setByTag();
-    }else if(getId() === true){
+    }else if (getId()) {
       setById();
     }else {
-      getAllClasses()
       setByClass(0);
     }
   };
 
-  var multipleTags = function() {
-    getTag();
-    getAllClasses();
+  var multiElements = function() {
     if(splitUp.length === 2){
-      matchTwoTags()
+      twoElements()
     }else{
-      console.log(splitUp.length)
-      matchThreeTags()
+      threeElements()
     }
   };
 
-  var matchTwoTags = function() {
-    if(getId() === true){
-      confirmIdMatch();
-    }else{
+  var twoElements = function() {
+    if(getId() && confirmIdMatch()){
+      setById();
+    }else if (arrOfClasses.length > 0){
       confirmClassMatch('tag');
     }
   };
 
-  var matchThreeTags = function(){
+  var threeElements = function(){
     getId();
-   if(confirmIdMatch() && confirmClassMatch('id') === true){
-    console.log('cool')
+   if(confirmIdMatch() && confirmClassMatch('id')){
    }
-  }
+  };
 
   var confirmIdMatch = function() {
     var idTagName = searchById().tagName.toUpperCase()
     if (tagName.toUpperCase() === idTagName){
       return true
     }else if (arrOfClasses.length > 0 ){
-      confirmClassMatch('id');
+      return confirmClassMatch('id');
     }
-  }
+  };
 
   var confirmClassMatch = function(compareWith) {
     var arrValue = arrOfClasses[0].slice(1)
     switch (compareWith) {
       case 'id':
-        if(classListToArray(searchById).indexOf(arrValue) > -1){
+        if (elementMatch(searchById, arrValue) === true){
           setById();
         };
         break;
       case 'tag':
-        if(classListToArray(searchByTag).indexOf(arrValue) > -1){
-         setByTag();
+        if (elementMatch(searchByTag, arrValue) === true){
+           setByTag();
         };
         break;
     }
-  }
+  };
+
+  var elementMatch = function(tagType, arrValue) {
+    if(classListToArray(tagType).indexOf(arrValue) > -1){
+      return true;
+    }
+  };
 
   var classListToArray = function(searchFunction) {
     var classArray = [], list = [];
-    if(searchFunction === searchById){
-      list = searchFunction().classList
-    }else{
-      list = searchFunction()[0].classList
-    }
+    list = getClassList(searchFunction)
     for(var i=0; i < list.length; i++){
       classArray.push(list[i]);
     }
     return classArray;
   };
 
- matchingRule()
-
-  var searchClass = function(className) {
-    if(elements.length === 0 && arrOfClasses.length > 0  ){
-      elements = document.getElementsByClassName(arrOfClasses[0].slice(1))
-      searchTag()
-    }else if (arrOfClasses.length > 0 ){
-      checkWithClasses()
+  var getClassList = function(searchFunction) {
+    if(searchFunction === searchById){
+      return searchFunction().classList
+    }else{
+      return searchFunction()[0].classList
     }
-  }
-
-  var searchElement = function() {
-    getTag()
-    getAllClasses()
-    for(var i=0; i < splitUp.length; i++){
-      if (splitUp[i].charAt(0) === '#'){
-        elements = document.getElementById(splitUp[i].slice(1))
-        searchClass()
-      }else{
-        searchClass()
-      }
-    };
   };
 
+  matchingRule()
 
-
-
-
-//   var findClassIndexes = function (){
-//     for(var i=0; i<selector.length;i++) {
-//       if (selector[i] === "."){
-//         classIdIndexPos.push(i);
-//       }
-//     }
-//   };
-
-//   var sliceUpSelector = function() {
-//     findIdIndex();
-//     findClassIndexes();
-//     if (classIdIndexPos.length > 0){
-//       checkStartPostition()
-//     };
-//     splitIdAndClasses()
-//     verifySelectorMatch();
-//   };
-
-//   var checkStartPostition = function() {
-//     classIdIndexPos = classIdIndexPos.sort(compareNumbers);
-//       if(classIdIndexPos[0] === 0){
-//        sliceAtEachPosition();
-//       }else{
-//         tagName = (selector.slice(0, classIdIndexPos[0]));
-//         sliceAtEachPosition();
-//       }
-//   };
-
-//   var sliceAtEachPosition = function() {
-//     var classToAdd = ''
-//     for (var i=0; i<classIdIndexPos.length;i++){
-//         classToAdd = selector.slice(classIdIndexPos[i], classIdIndexPos[i+1]);
-//         selectorArray.push(classToAdd)
-//     }
-//     console.log(String(classToAdd))
-//   };
-
-//   var verifySelectorMatch = function() {
-//     if (classNames.length === 0 && idName === ''){
-//       elements = (document.getElementsByTagName(selector))
-//     }else{
-//       checkWithId();
-//       checkWithClass();
-//     };
-//   };
-
-//   var splitIdAndClasses = function() {
-//     for (var i=0; i < selectorArray.length; i++){
-//       if (selectorArray[i].charAt(0) === '#')
-//       {
-//         idName = selectorArray.splice(i, 1)
-//         idName = String(idName).slice(1)
-//       }
-//     }
-//     classNames = selectorArray.join('').slice(1).split('.')
-//   }
-
-//   var checkWithId = function() {
-//     if (idName != ''){
-//       idElement = (document.getElementById(idName))
-//       if (tagName.toUpperCase() === idElement.tagName.toUpperCase() || tagName === ''){
-//         elements.push(idElement)
-//         foundById = true;
-//       }
-//     }
-//   };
-
-//   var checkWithClass = function() {
-//     var matches = 0
-//     if(foundById === true && classNames.length > 0){
-//       if(tagName.toUpperCase() === idElement.tagName.toUpperCase() || tagName === ''){
-//         for(var i=0; i < classNames.length;i++){
-//           var currentClass = classNames[i]
-//           for(var n=0; n < elements[0].classList.length; n++){
-
-//             if(currentClass === elements[0].classList[n]){
-//               matches++
-//             }
-//           }
-//         }
-//       }else{
-
-//         elements = []
-//       }
-
-//     }else if(foundById === false && classNames.length > 0){
-//       console.log('toodles')
-//       var arrr = 'some_class'
-
-//       bb = 'some_class'
-//       var elementByClass = []
-//       elementByClass.push(document.getElementsByClassName(arrr))
-//       console.log('bbbb ' + elementByClass[0])
-//       console.log('some_class')
-//       //for(var i=0; i < elementByClass.length; i++){
-
-//       //}
-
-//       //if(tagName.toUpperCase() === elementByClass[i].tagName.toUpperCase() || tagName === ''){
-
-//       //}
-//       matches = classNames.length
-//     }
-//     if(matches != classNames.length ){
-//       elements = []
-//     }
-//   };
-
-// // var elements2 = (document.getElementsByClassName(className));
-
-// // elements.push(document.getElementById('some_id'));
-// // var checkClassList = function () {
-
-// // }
-// // findIdIndex()
-// // findClassIndexes()
-// //sliceUpSelector()
-// console.log('sss: ' + selectorArray)
-// // console.log('elements: ' + elements)
-
-// console.log('cc: ' + elements)
-// var ff = []
-// var a = 'me'
-// ff.push(selectorArray[0])
-// var ss = new String(classNames[0])
-// var hh = ['.some_class', '.another_class']
-// hh = hh.toString().replace(/\,/g,'').slice(1).split('.')
- var ee = ['.some_class', '.another_class']
- ee = ee.join('').slice(1).split('.')
-// var bb = 'some_class'
- var dd = ''
-dd = (document.getElementsByClassName(ee[0]))
- console.log('ff: ' + dd[0].nodeName)
-// console.log(ss.slice(1))
-// console.log(dd[0].tagName)
-console.log('---------------------------')
-//console.log(elements[0].classList)
-//console.log(elements[0].tagName)
-// console.log('id name: ' + idName)
-// console.log('class name: ' + classNames)
-// console.log('tag name: ' + tagName)
-
-  ///////////////////
-  // Your code here //
-  ////////////////////
   return elements;
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ###Approach
-split the selector into it's 'type', 'id' and 'class'
-created an order of precendence to search from id, class, type
-for each selection compare the returned type, id and class with the selector to confirm a match
+* First step was to split the selector by tag-name, id and class into an array so I could access each element.
+* Next I wanted to know what each element was so I put them in specified variables for tagName, idName and an arrayOfClasses. The reasoning here was I can then use the correct 'getElement' method and it made it clear what elements I had.
+* Based on the number of elements in the split up selector I had a set of rules to pull back the DOM element.
+    - One element rule just brings back that DOM element.
+    - Two elements would use the id to select if present and then compare it these attributes to the other element. If no id then tagName was used and compared against the classList to the class in arrayOfClasses.
+    - Three elements used id to get the DOM element then compared the DOM's attributes to the tagName and arrayOfClasses.
+
+###Improvements
+All the the tests pass but 
